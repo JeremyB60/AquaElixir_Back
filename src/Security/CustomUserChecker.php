32b4b586ca7410d5cbf398ea1b58ffb5c108e0a4
Morgan-Tln <morgan.tilquin@epitech.eu>
2023-11-24 14:16:46 +0100

    Login fix / Regex / Confirm password/ ConflitFix

---
+++ src/Security/CustomUserChecker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use App\Entity\User;
+
+class CustomUserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->getIsEmailConfirmed()) {
+            throw new CustomUserMessageAuthenticationException('Votre email n\'a pas été confirmé.');
+        }
+
+        // Vérifier si le compte est suspendu
+        if ($user->getAccountStatus() === 'suspend') {
+            throw new CustomUserMessageAuthenticationException('Votre compte est suspendu.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+    }
+}
